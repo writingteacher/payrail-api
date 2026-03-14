1168a7a350c5d33ae3fc7f82563d4bd266f64809
eslwriter <46510843+writingteacher@users.noreply.github.com>
2026-03-14 18:49:49 +0900

    add database connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./src/app');
 const dotenv = require('dotenv');
+dotenv.config({ path: require('path').resolve(__dirname, '.env') });
 
-dotenv.config();
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
